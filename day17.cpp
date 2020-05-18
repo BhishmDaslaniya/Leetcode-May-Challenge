eb56e1f99f7ed47ab239635740ff772b90cd6c67
Bhishm Daslaniya <bhishm1999@gmail.com>
2020-05-18 10:12:38 +0530

    Create day17.cpp

---
+++ day17.cpp
@@ -0,0 +1,31 @@
+/*
+    Probelm :Find All Anagrams in a String
+    Author  : Bhishm Daslaniya
+    "Make it work, make it right, make it fast." 
+                                                – Kent Beck  
+*/
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> result;
+        if (p.empty() || s.empty()) {
+            return result;
+        }
+
+        vector<int> cnts(26);
+        for (const auto& c : p) {
+            ++cnts[c - 'a'];
+        }
+        
+        for (int left = 0, right = 0; right < s.length(); ++right) {
+            --cnts[s[right] - 'a'];
+            while (left <= right && cnts[s[right] - 'a'] < 0) {
+                ++cnts[s[left++] - 'a'];
+            }
+            if (right - left + 1 == p.length()) {
+                result.emplace_back(left);
+            }
+        }
+        return result;
+    }
+};
